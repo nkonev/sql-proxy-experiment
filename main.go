76b6f7e99ae081e8449d3e1caf1a8abca58ea1b0
Конев Никита Дмитриевич <nkonev@samokat.ru>
2024-10-01 14:17:29 +0300

    proxy.RegisterTracer()

--- main.go
+++ main.go
@@ -1,64 +1,60 @@
 package main
 
 import (
-	"context"
 	"database/sql"
-	"database/sql/driver"
-	"fmt"
+	_ "github.com/mattn/go-sqlite3"
+	proxy "github.com/shogo82148/go-sql-proxy"
 	"log"
-	"strings"
-	"time"
-
-	"github.com/mattn/go-sqlite3"
-	"github.com/shogo82148/go-sql-proxy"
 )
 
-func printArgs(args []driver.NamedValue) string {
-	sb := strings.Builder{}
-	sb.WriteString("[")
-	for idx, nv := range args {
-		if idx != 0 {
-			sb.WriteString(", ")
-		}
-		sb.WriteString(fmt.Sprintf("{%v $%d %v}", nv.Name, nv.Ordinal, nv.Value))
-	}
-	sb.WriteString("]")
-	return sb.String()
-}
+//func printArgs(args []driver.NamedValue) string {
+//	sb := strings.Builder{}
+//	sb.WriteString("[")
+//	for idx, nv := range args {
+//		if idx != 0 {
+//			sb.WriteString(", ")
+//		}
+//		sb.WriteString(fmt.Sprintf("{%v $%d %v}", nv.Name, nv.Ordinal, nv.Value))
+//	}
+//	sb.WriteString("]")
+//	return sb.String()
+//}
 
 func main() {
-	driverName := "sqlite3-proxy"
-	sql.Register(driverName, proxy.NewProxyContext(&sqlite3.SQLiteDriver{}, &proxy.HooksContext{
-		PreExec: func(_ context.Context, _ *proxy.Stmt, _ []driver.NamedValue) (interface{}, error) {
-			// The first return value(time.Now()) is passed to both `Hooks.Exec` and `Hook.ExecPost` callbacks.
-			return time.Now(), nil
-		},
-		PostExec: func(_ context.Context, ctx interface{}, stmt *proxy.Stmt, args []driver.NamedValue, _ driver.Result, _ error) error {
-			// The `ctx` parameter is the return value supplied from the `Hooks.PreExec` method, and may be nil.
-			log.Printf(fmt.Sprintf("Exec query: %s; args = %v (%s)\n", stmt.QueryString, printArgs(args), time.Since(ctx.(time.Time))))
-			return nil
-		},
-		PrePrepare: func(_ context.Context, _ *proxy.Stmt) (interface{}, error) {
-			// The first return value(time.Now()) is passed to both `Hooks.Exec` and `Hook.ExecPost` callbacks.
-			return time.Now(), nil
-		},
-		PostPrepare: func(_ context.Context, ctx interface{}, stmt *proxy.Stmt, _ error) error {
-			// The `ctx` parameter is the return value supplied from the `Hooks.PreExec` method, and may be nil.
-			log.Printf(fmt.Sprintf("Prepare query: %s (%s)\n", stmt.QueryString, time.Since(ctx.(time.Time))))
-			return nil
-		},
-		PreQuery: func(_ context.Context, _ *proxy.Stmt, _ []driver.NamedValue) (interface{}, error) {
-			// The first return value(time.Now()) is passed to both `Hooks.Exec` and `Hook.ExecPost` callbacks.
-			return time.Now(), nil
-		},
-		PostQuery: func(_ context.Context, ctx interface{}, stmt *proxy.Stmt, args []driver.NamedValue, _ driver.Rows, _ error) error {
-			// The `ctx` parameter is the return value supplied from the `Hooks.PreExec` method, and may be nil.
-			log.Printf(fmt.Sprintf("Query query: %s; args = %v (%s)\n", stmt.QueryString, printArgs(args), time.Since(ctx.(time.Time))))
-			return nil
-		},
-	}))
+	//driverName := "sqlite3-proxy"
+	//sql.Register(driverName, proxy.NewProxyContext(&sqlite3.SQLiteDriver{}, &proxy.HooksContext{
+	//	PreExec: func(_ context.Context, _ *proxy.Stmt, _ []driver.NamedValue) (interface{}, error) {
+	//		// The first return value(time.Now()) is passed to both `Hooks.Exec` and `Hook.ExecPost` callbacks.
+	//		return time.Now(), nil
+	//	},
+	//	PostExec: func(_ context.Context, ctx interface{}, stmt *proxy.Stmt, args []driver.NamedValue, _ driver.Result, _ error) error {
+	//		// The `ctx` parameter is the return value supplied from the `Hooks.PreExec` method, and may be nil.
+	//		log.Printf(fmt.Sprintf("Exec query: %s; args = %v (%s)\n", stmt.QueryString, printArgs(args), time.Since(ctx.(time.Time))))
+	//		return nil
+	//	},
+	//	PrePrepare: func(_ context.Context, _ *proxy.Stmt) (interface{}, error) {
+	//		// The first return value(time.Now()) is passed to both `Hooks.Exec` and `Hook.ExecPost` callbacks.
+	//		return time.Now(), nil
+	//	},
+	//	PostPrepare: func(_ context.Context, ctx interface{}, stmt *proxy.Stmt, _ error) error {
+	//		// The `ctx` parameter is the return value supplied from the `Hooks.PreExec` method, and may be nil.
+	//		log.Printf(fmt.Sprintf("Prepare query: %s (%s)\n", stmt.QueryString, time.Since(ctx.(time.Time))))
+	//		return nil
+	//	},
+	//	PreQuery: func(_ context.Context, _ *proxy.Stmt, _ []driver.NamedValue) (interface{}, error) {
+	//		// The first return value(time.Now()) is passed to both `Hooks.Exec` and `Hook.ExecPost` callbacks.
+	//		return time.Now(), nil
+	//	},
+	//	PostQuery: func(_ context.Context, ctx interface{}, stmt *proxy.Stmt, args []driver.NamedValue, _ driver.Rows, _ error) error {
+	//		// The `ctx` parameter is the return value supplied from the `Hooks.PreExec` method, and may be nil.
+	//		log.Printf(fmt.Sprintf("Query query: %s; args = %v (%s)\n", stmt.QueryString, printArgs(args), time.Since(ctx.(time.Time))))
+	//		return nil
+	//	},
+	//}))
 
-	db, err := sql.Open(driverName, ":memory:")
+	proxy.RegisterTracer()
+
+	db, err := sql.Open("sqlite3:trace", ":memory:")
 	if err != nil {
 		log.Fatalf("Open filed: %v", err)
 	}
